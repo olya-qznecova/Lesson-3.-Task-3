76bcabbb3c18bd8cc1df4a20b57b561ca819decf
olya-qznecova <35519547+olya-qznecova@users.noreply.github.com>
2018-02-22 10:26:45 +0300

    обновлены файлы из третьего задания
    добавлена опция выбора города из списка

--- code.js
+++ code.js
@@ -1,4 +1,12 @@
-function CheckData()
+// Урок 3//
+function ButtonSetDefaultValue () {
+    var buttonEl = document.getElementById('checkButton');
+    buttonEl.addEventListener('click', function (event) { //отменяем действия браузера по умолчанию, при клике форма не отправляется
+        event.preventDefault();
+    });
+}
+
+checkButton.onclick = function CheckData()
 {
     CheckElement("name",/[a-zа-я]+$/,"Неверное имя!");
 
@@ -6,12 +14,13 @@ function CheckData()
     CheckElement("email",regEmail, "Неверный Email!");
 
     CheckElement("phone",/^\+7\(\d{3}\)\d{3}-\d{4}/, "Неверный номер телефона!");
-}
+
+};
 
 function CheckElement(id, reg, errorText)
 {
     var elem = document.getElementById(id);
-    if (elem==null)
+     if (elem==null)
     {
         //error;
     }
@@ -23,11 +32,28 @@ function CheckElement(id, reg, errorText)
             showError(elem, errorText);
 
     }
-}
+  }
 
 function showError(elem, errorText) {
     elem.className="error";
     var txt = document.createTextNode(errorText);
     txt.className="error";
     elem.parentNode.appendChild(txt);
+
 }
+// Урок 4. Задание 2.получаем список городов после загрузки страницы
+(function ($) {
+    $(function() {
+           $.ajax({
+                url: 'https://jsonplaceholder.typicode.com/users',
+                dataType: 'json',
+                success: function (users) {
+                    $.each(users, function (i, user) {
+                        var $option = $('<option />').text(user.address.city);
+                        $('.cities').append($option);
+                    });
+
+                }
+            })
+        });
+})(jQuery);
